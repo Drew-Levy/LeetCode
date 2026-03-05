3ad332f6d925ed89cd7ac2d917386931fc1467d3
Drew-Levy <dslevy@uci.edu>
2026-03-04 22:32:43 -0800

    Create Sqrt(x).cpp

---
+++ solves/Sqrt(x).cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x <2) return x;
+        int left = 1;
+        int right = x >> 1;
+        while(left <=right){
+            int mid = (left+right) >> 1;
+            int square = mid*mid;
+
+            if(square==x) return mid;
+            else if(square <x) left=mid+1;
+            else right = mid-1;
+        }
+        return right;
+    }
+};
