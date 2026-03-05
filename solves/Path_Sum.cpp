3290c510a741aa26cbbe454518aa20699c06b5b4
Drew-Levy <dslevy@uci.edu>
2026-03-04 23:08:17 -0800

    Update Path_Sum.cpp

--- solves/Path_Sum.cpp
+++ solves/Path_Sum.cpp
@@ -13,7 +13,7 @@ class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
         if(!root) return false;
-        if(root->val == targetSum && root->left == NULL && root->val == targetSum && root->right == NULL) return true;
+        if(root->val ==targetSum && root->left == NULL && root->right == NULL) return true;
         return hasPathSum(root->left, targetSum-root->val) || hasPathSum(root->right, targetSum-root->val);
     }
 };
