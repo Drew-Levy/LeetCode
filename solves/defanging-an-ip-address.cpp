e91b87f08d2f83447b8a74ccb068e47cbbda7c6c
Drew-Levy <dslevy@uci.edu>
2026-03-05 22:10:09 -0800

    Create defanging-an-ip-address.cpp

---
+++ solves/defanging-an-ip-address.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string ret;
+        for(auto c: address){
+            if(c == '.') ret+="[.]";
+            else ret+=c;  
+        }
+        return ret;
+    }
+};
