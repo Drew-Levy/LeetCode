2f50d7ea2bea72dc72c6bd6799b3226b92dfadb5
Drew-Levy <dslevy@uci.edu>
2026-03-08 14:24:58 -0700

    Create TwoSumII.cpp

---
+++ solves/TwoSumII.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> ret;
+        int l =0, r = numbers.size()-1;
+
+        while(l<r){
+            int sum = numbers[l] + numbers[r];
+            if(sum==target){
+                ret.push_back(l+1);
+                ret.push_back(r+1);
+                break;
+            }
+            else if(sum<target) ++l;
+            else --r;
+        }
+        return ret;
+    }
+};
