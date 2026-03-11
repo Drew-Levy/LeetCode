b0fdfbbcb70d45b2f52c07700539d952cdefd64d
Drew-Levy <dslevy@uci.edu>
2026-03-10 20:23:28 -0700

    Create find-n-unique-integers-sum-up-to-zero.cpp

---
+++ Easy/find-n-unique-integers-sum-up-to-zero.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        vector<int> ret;
+        if(n%2==1) ret.push_back(0);
+        for(int i=1; i<=n/2; ++i){
+            ret.push_back(i);
+            ret.push_back(-i);
+        }
+        return ret;
+    }
+};
