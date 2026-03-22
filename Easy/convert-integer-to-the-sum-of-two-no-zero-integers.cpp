35a1fca0db7639c189ce470767d4693c806fa1d7
Drew-Levy <dslevy@uci.edu>
2026-03-21 20:24:40 -0700

    Create convert-integer-to-the-sum-of-two-no-zero-integers.cpp

---
+++ Easy/convert-integer-to-the-sum-of-two-no-zero-integers.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> getNoZeroIntegers(int n) {
+        for(int i =1; i< n; ++i){
+            int left = i;
+            int right = n-i;
+            if((to_string(left) +to_string(right)).find('0')==string::npos) return {left, right};
+        }
+        return {};
+    }
+};
