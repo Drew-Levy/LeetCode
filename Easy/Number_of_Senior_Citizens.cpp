8fd315046331b9cc1167cae83a710299cd34f6c8
Drew-Levy <dslevy@uci.edu>
2026-03-22 12:50:04 -0700

    Create Number_of_Senior_Citizens.cpp

---
+++ Easy/Number_of_Senior_Citizens.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int old =0;
+
+        for(int i=0; i< details.size(); ++i){
+            int age = (details[i][11]- '0')*10 + (details[i][12] - '0');
+            cout<<age;
+            if(age > 60) ++old;
+        }
+        return old;
+    }
+};
